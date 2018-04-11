3eaccea67cd347efa78867d0809c7da3ceed0a09
Hongwei (Henry) Zhou <hz1101@nyu.edu>
2018-04-11 11:43:09 -0400

    Added general game state class

--- win32/SMWGameState.h
+++ win32/SMWGameState.h
@@ -1,41 +1,10 @@
-#include <map>
-#include <set>
-#include <vector>
-#include <string>
-#include <sstream>
-#include <algorithm>
-#include <assert.h>
-#include <ctype.h>
-#include <iostream>
-#include <fstream>
+#include "GameState.h"
 
-#include "./port.h"
-#include "./statemanager.h"
-#include "./snapshot.h"
-#include "memmap.h"
-#include "wsnes9x.h"
-#define ANNOYING_STATE_CLOCK 100
-
-class SMWGameState {
+class SMWGameState : GameState {
 public:
-	SMWGameState() {
-		//if (!stateMan.init(1024 * 1024 * 1024)) // 1 MB of buffer size
-		//	fprintf(stdout, "\n INSUFFICIENT MEM SIZE IN STATEMAN \n");
-	}
 
-	void loadState() {
-		std::stringstream temp;
-		temp << ss.str();
-		if (!UnfreezeState(ss))
-			std::cout << "yer dun goofed" << std::endl;
-		ss.swap(temp);
-		}
-
-	void saveState() {
-		ss = FreezeState();
-	}
-
-	void updateState() {
+	void updateState() override {
+		//~~~~~~~~~~~~~~~~~~~~~~~~~Automatically Load Save Slot~~~~~~~~~~~~~~~~~~~~~~~~
 		if (!levelLoaded) { 
 			/* string into TCHAR array*/
 			//std::string str_dir = "C:\\Users\\Henry\\Documents\\GitHub\\snes9x\\win32\\Saves\\Super Mario World (U) [!].srm";
@@ -44,7 +13,7 @@ public:
 			//std::copy(str_dir.begin(), str_dir.end(), dir);
 
 			//LoadROM(dir, NULL);
-			FreezeUnfreezeSlot(0, false); 
+			FreezeUnfreezeSlot(2, false); 
 			levelLoaded = true;
 			saveState();
 			//Settings.FrameTime = 500;//4000;//FrameTimings[0];
@@ -60,15 +29,17 @@ public:
 		death = playerPosY > 60000;//(Memory.RAM[0xDDA] == 0xFF && playerPosY > 6000);
 		levelComplete = (Memory.RAM[0xDDA] == 0xFF && Memory.RAM[0x13D9] == 2);
 
+		/*~~~~~~~~~~~~~~~~~~Below is loading the same state multiple times~~~~~~~~~~~~~~~~~~~~~~
 		clocktick++;
 		if (clocktick >= ANNOYING_STATE_CLOCK) {
 			//loadState();
 			clocktick = 0;
 			save = !save;
 		}
+			*/
 	}
 
-	void printState() {
+	void printState() override {
 		fprintf(stdout, "PosX << %d PosY << %d Score << %d Timer << %d Death << %d levelComplete << %d\n", playerPosX, playerPosY, marioScore, timer, death, levelComplete);
 	}
 
@@ -86,6 +57,4 @@ private:
 
 	int clocktick = 0;
 	bool save = false;
-
-	stringstream ss;
 };
